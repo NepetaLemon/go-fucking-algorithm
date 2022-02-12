86e29fe14bc67caaa566be089333846dee7c8479
nepetalemon <nepetalemon@hotmail.com>
2022-02-13 00:01:56 +0800

    [ADD] 环形链表

---
+++ linked-list-cycle-141/linked-list-cycle.go
@@ -0,0 +1,18 @@
+package linked_list_cycle_141
+
+import (
+	. "go-fucking-algorithm/listnode"
+)
+
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+
+	return false
+}
